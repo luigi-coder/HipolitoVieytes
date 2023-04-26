0c07defe3509cf6acd0bbcc87ace844df1b03066
luigi-coder <luigicoder28@gmail.com>
2023-04-26 17:19:09 -0300

    Modulos internos y externos

---
+++ DW4_MARTES/CLASE_3/calcular.js
@@ -0,0 +1,13 @@
+const suma = (a,b)=> a+b;
+const resta = (a,b)=> a-b;
+const multiplicacion = (a,b)=> a*b;
+const division = (a,b)=> a/b;
+                       
+// finalmente, exportamos la función
+module.exports = {
+    suma,
+    resta,
+    multiplicacion,
+    division,
+}
+                    
---
+++ DW4_MARTES/CLASE_3/index.js
@@ -0,0 +1,36 @@
+const operaciones = require('./calcular');
+
+console.log(operaciones.suma(2,3));
+console.log(operaciones.resta(2,3));
+console.log(operaciones.multiplicacion(2,3));
+console.log(operaciones.division(2,3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* const cowsay = require('cowsay');
+
+const objetoCowsay = {
+
+    text: "Hola mundo desde Cowsay",
+    e: "oo",
+    T: "UU"
+}
+
+console.log(cowsay.say(objetoCowsay));
+
+
+console.log(cowsay.say({
+    text: "Hola mundo desde Cowsay",
+    e: "o0",
+    T: "U"
+})) */
