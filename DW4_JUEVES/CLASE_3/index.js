846563a83bcfa077debe3303f190dc6a0704cc6e
luigi-coder <luigicoder28@gmail.com>
2023-05-01 18:19:02 -0300

    Modulos internos y externos

---
+++ DW4_JUEVES/CLASE_3/index.js
@@ -0,0 +1,22 @@
+const metodosOp = require('./calcular');
+
+const sumar = metodosOp.suma(10,10);
+
+console.log(`
+ Suma de 10 + 10 = ${sumar}
+`);
+
+
+/* const COWSAY = require('cowsay'); importar
+
+uso a los metodos 
+const objVaca = {
+    text: "Modulos desde node",
+    e : "0o",
+    T : "UU",
+}
+const final = COWSAY.say(objVaca)
+console.log(final);
+ */
+
+
