076c822ab663851d1b4d1c500e269a4e986e480a
luigi-coder <luigicoder28@gmail.com>
2023-05-03 19:37:17 -0300

    Protocolo http, fetch y promesas

--- DW3_MIERCOLES/CLASE_3/main.js
+++ DW3_MIERCOLES/CLASE_3/main.js
@@ -1,29 +1,93 @@
-document.getElementById("myForm").addEventListener("submit", function(event){
-    event.preventDefault();
+document.getElementById("myForm").addEventListener("submit", (e)=>{
+
+    e.preventDefault();
+
     let nombre = document.getElementById("nombre").value;
     let email = document.getElementById("email").value;
     let telefono = document.getElementById("telefono").value;
     let sitioWeb = document.getElementById("sitio").value;
     let asunto = document.getElementById("asunto").value;
     let mensaje = document.getElementById("mensaje").value;
-    let datos = {
-        nombre: nombre,
-        email: email,
-        telefono: telefono,
-        sitioWeb: sitioWeb,
-        asunto: asunto,
-        mensaje: mensaje
+
+    const datos = {
+        nombre,
+        email,
+        telefono,
+        sitioWeb,
+        asunto,
+        mensaje    
     }
+    
     fetch('https://jsonplaceholder.typicode.com/posts', {
+
         method: 'POST',
         body: JSON.stringify(datos),
         headers: {
-            "Content-type": "application/json; charset=UTF-8"
+            "Content-Type": "application/json; charset=UTF-8"
         }
     })
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-        document.getElementById("datos").innerHTML = JSON.stringify(json);
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
     })
+
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* fetch('https://pokeapi.co/api/v2/pokemon')
+.then((res)=> res.json())
+.then((data)=> console.log(data));  */
+
+
+// Las promesas sirven para consumir una API
+// o para hacer peticiones http
+
+/* function saludar(nombre){
+
+    return new Promise((resolve, reject)=>{
+        if(nombre === "santiago" || nombre === "SANTIAGO"){
+            resolve("Bienvenido " + nombre);
+        }else if(nombre === ""){
+            reject("No ingreso un nombre");
+        }else {
+            reject("No sos bienvenido");
+        }
+    });
+}
+
+saludar("Luis")
+    .then((mensaje)=>{
+        console.log(mensaje);
+    })
+    .catch((mensaje)=>{
+        console.log(mensaje);
+    }); 
+
+
+ */
